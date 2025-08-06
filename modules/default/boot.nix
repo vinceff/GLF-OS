0b858354400a6420d6ad22d09adb2b14fa428f5c
Vinceff <101025517+vinceff@users.noreply.github.com>
2025-08-06 15:21:41 +0200

    Update boot.nix

--- modules/default/boot.nix
+++ modules/default/boot.nix
@@ -1,59 +1,77 @@
-config = lib.mkIf config.glf.boot.enable {
-  nixpkgs.overlays = [
-    (self: super: {
-      linuxPackages = super.linuxPackages // {
-        kernel = super.linuxPackages.kernel.override {
-          structuredExtraConfig = with lib.kernel; {
-            HZ_1000 = yes;
-            HZ = 1000;
-            PREEMPT_FULL = yes;
-            IOSCHED_BFQ = yes;
-            DEFAULT_BFQ = yes;
-            DEFAULT_IOSCHED = "bfq";
-            V4L2_LOOPBACK = module;
-            HID = yes;
+{
+  lib,
+  config,
+  pkgs,
+  pkgs-unstable,
+  ...
+}:
+let
+  plymouth-glfos = pkgs.callPackage ../../pkgs/plymouth-glfos {};
+in
+{
+  options.glf.boot.enable = lib.mkOption {
+    description = "Enable GLF Boot configurations";
+    type = lib.types.bool;
+    default = true;
+  };
+
+  config = lib.mkIf config.glf.boot.enable {
+    nixpkgs.overlays = [
+      (self: super: {
+        linuxPackages = super.linuxPackages // {
+          kernel = super.linuxPackages.kernel.override {
+            structuredExtraConfig = with lib.kernel; {
+              HZ_1000 = yes;
+              HZ = 1000;
+              PREEMPT_FULL = yes;
+              IOSCHED_BFQ = yes;
+              DEFAULT_BFQ = yes;
+              DEFAULT_IOSCHED = "bfq";
+              V4L2_LOOPBACK = module;
+              HID = yes;
+            };
           };
         };
+      })
+    ];
+
+    services.udev.extraRules = ''
+      ACTION=="add|change", SUBSYSTEM=="block", ATTR{queue/scheduler}="bfq"
+    '';
+
+    boot.loader.grub.splashImage = ../../assets/wallpaper/dark.jpg;
+    boot.loader.grub.default = "saved";
+    boot = {
+      kernelPackages = pkgs-unstable.linuxPackages_latest;
+      tmp.cleanOnBoot = true;
+      supportedFilesystems.zfs = lib.mkForce false;
+      kernelParams =
+        if builtins.elem "kvm-amd" config.boot.kernelModules then [ "amd_pstate=active" "nosplit_lock_mitigate" "clearcpuid=514" ] else [ "nosplit_lock_mitigate" ];
+      plymouth = {
+        enable = true;
+        theme = "glfos";
+        themePackages = [ plymouth-glfos ];
       };
-    })
-  ];
+      kernel.sysctl = {
+        "kernel.split_lock_mitigate" = 0;
+        vm_swappiness = 10;
+        vm_vfs_cache_pressure = 50;
+        vm_dirty_bytes = 268435456;
+        "vm.max_map_count" = 16777216;
+        vm_dirty_background_bytes = 67108864;
+        vm_dirty_writeback_centisecs = 1500;
+        kernel_nmi_watchdog = 0;
+        kernel_unprivileged_userns_clone = 1;
+        kernel_printk = "3 3 3 3";
+        kernel_kptr_restrict = 2;
+        kernel_kexec_load_disabled = 1;
+      };
+    };
 
-  services.udev.extraRules = ''
-    ACTION=="add|change", SUBSYSTEM=="block", ATTR{queue/scheduler}="bfq"
-  '';
-
-  boot.loader.grub.splashImage = ../../assets/wallpaper/dark.jpg;
-  boot.loader.grub.default = "saved";
-  boot = {
-    kernelPackages = pkgs-unstable.linuxPackages_latest;
-    tmp.cleanOnBoot = true;
-    supportedFilesystems.zfs = lib.mkForce false;
-    kernelParams =
-      if builtins.elem "kvm-amd" config.boot.kernelModules then [ "amd_pstate=active" "nosplit_lock_mitigate" "clearcpuid=514" ] else [ "nosplit_lock_mitigate" ];
-    plymouth = {
+    hardware.graphics = {
       enable = true;
-      theme = "glfos";
-      themePackages = [ plymouth-glfos ];
-    };
-    kernel.sysctl = {
-      "kernel.split_lock_mitigate" = 0;
-      vm_swappiness = 10;
-      vm_vfs_cache_pressure = 50;
-      vm_dirty_bytes = 268435456;
-      "vm.max_map_count" = 16777216;
-      vm_dirty_background_bytes = 67108864;
-      vm_dirty_writeback_centisecs = 1500;
-      kernel_nmi_watchdog = 0;
-      kernel_unprivileged_userns_clone = 1;
-      kernel_printk = "3 3 3 3";
-      kernel_kptr_restrict = 2;
-      kernel_kexec_load_disabled = 1;
+      package = pkgs-unstable.mesa;
+      package32 = pkgs-unstable.pkgsi686Linux.mesa;
     };
   };
-
-  hardware.graphics = {
-    enable = true;
-    package = pkgs-unstable.mesa;
-    package32 = pkgs-unstable.pkgsi686Linux.mesa; 
-  };
-};
+}
