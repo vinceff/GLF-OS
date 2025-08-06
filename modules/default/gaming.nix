917712ac3301207888a3eb3c5439d9d9b7a046fe
Vinceff <101025517+vinceff@users.noreply.github.com>
2025-08-06 15:47:03 +0200

    Update gaming.nix

--- modules/default/gaming.nix
+++ modules/default/gaming.nix
@@ -29,7 +29,7 @@ in
       glxinfo          # Show hardware information
       heroic           # Native GOG, Epic, and Amazon Games Launcher for Linux, Windows and Mac
       joystickwake     # Joystick-aware screen waker
-      linuxKernel.packages.linux_6_12.hid-tmff2
+     # linuxKernel.packages.linux_6_12.hid-tmff2
       mangohud         # Vulkan and OpenGL overlay for monitoring FPS, temperatures, CPU/GPU load and more
       #mesa             # Ensure last mesa stable on GLF OS
       #oversteer        # Steering Wheel Manager for Linux
